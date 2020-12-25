988e417816e0f2c7e0402ff99dbcdb139613dee2
Mensur Bukvarevic <mbukvarevic@gmail.com>
2020-12-25 13:54:52 +0100

    Sign-In & Sign-Out Methods Complete

--- app/src/main/java/at/fh/swengb/beFast/ui/more/MoreFragment.kt
+++ app/src/main/java/at/fh/swengb/beFast/ui/more/MoreFragment.kt
@@ -1,10 +1,10 @@
 package at.fh.swengb.beFast.ui.more
 
-import android.app.Activity
-import android.app.Application
-import android.content.Context
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,21 +14,30 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import at.fh.swengb.beFast.R
-import at.fh.swengb.beFast.ui.news.NewsFragment
+import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.dynamite.DynamiteModule.load
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.fragment_more.*
+import java.lang.System.load
+import java.util.ServiceLoader.load
 
 
 class MoreFragment : Fragment() {
 
     private lateinit var moreViewModel: MoreViewModel
+    companion object {
+        val RC_SIGN_IN = 0
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
 
     ): View? {
         moreViewModel =
@@ -42,13 +51,14 @@ class MoreFragment : Fragment() {
     }
     // create google sign in and google sign in client
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        val RC_SIGN_IN = 0
         super.onActivityCreated(savedInstanceState)
+
+        //create sign in
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
         val mGoogleSignInClient = activity?.let { GoogleSignIn.getClient(it, gso) }
-
+        //sign in method
         fun signIn() {
             val signInIntent: Intent
             if (mGoogleSignInClient != null) {
@@ -57,10 +67,33 @@ class MoreFragment : Fragment() {
             }
 
         }
+        //signOut method
+        fun signOut() {
+            if (mGoogleSignInClient != null) {
+                activity?.let {
+                    mGoogleSignInClient.signOut()
+                            .addOnCompleteListener(it, OnCompleteListener<Void?> {
+                                Log.i("MoreFragment", "SignedOut")
+                                onStart()
+                                updateUI(account = null)
+
+                            })
+                }
+
+                //updateUI(account = null)
+            }
+        }
+
+
 
         sign_in_button.setOnClickListener {
             signIn()
         }
+
+        sign_out_button.setOnClickListener {
+            signOut()
+        }
+
 
 
 
@@ -68,10 +101,18 @@ class MoreFragment : Fragment() {
     // check if account signed in or not
     fun updateUI(account: GoogleSignInAccount?) {
         if (account != null) {
-            // TODO: hide log-in interface and show username
-            Toast.makeText(activity, "Already signed in", Toast.LENGTH_LONG).show()
+
+            sign_in_button.setVisibility(View.GONE)
+            login_name.text = account.displayName
+            login_email.text = account.email
+
+
         } else {
-            Toast.makeText(activity, "Sign in", Toast.LENGTH_LONG).show()
+            sign_out_button.setVisibility(View.GONE)
+            login_name.setVisibility(View.GONE)
+            login_email.setVisibility(View.GONE)
+            logged_in_as.setVisibility(View.GONE)
+
         }
     }
     // call the updateUI function when starting
@@ -81,6 +122,29 @@ class MoreFragment : Fragment() {
         updateUI(account)
     }
 
-    // sign-in method
+    // handle the sign in result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === RC_SIGN_IN && resultCode == RESULT_OK) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
 
 }
+
+//TODO: update fragment when sign-in and sign-out process is complete
