95fa32358522c0cfbfeccf4357751d03641710d0
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2020-12-26 15:08:43 +0100

    ui fixed

---
+++ app/src/main/java/at/fh/swengb/beFast/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package at.fh.swengb.beFast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
