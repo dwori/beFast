aeeeb9b93737d5282866b7f9bb2ab17e112d88bb
domiK66 <73217569+domiK66@users.noreply.github.com>
2021-01-06 18:58:08 +0100

    Revert "Brands & Settings fixes"

--- app/src/main/java/at/fh/swengb/beFast/ui/brands/BrandsFragment.kt
+++ app/src/main/java/at/fh/swengb/beFast/ui/brands/BrandsFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -52,10 +51,6 @@ class BrandsFragment : Fragment() {
 
         if (sharedPreferences.getBoolean(SettingsActivity.loginBool, false)) {
 
-            brands_info.visibility = View.GONE
-
-            save_brands.visibility = View.VISIBLE
-
             switch1.isChecked = sharedPreferences.getBoolean(nikeKey, true)
 
             switch2.isChecked = sharedPreferences.getBoolean(adidasKey, true)
@@ -79,8 +74,6 @@ class BrandsFragment : Fragment() {
             }
 
         } else {
-            brands_info.visibility = View.VISIBLE
-            save_brands.visibility = View.GONE
             switch1.visibility = View.GONE
             switch2.visibility = View.GONE
             switch3.visibility = View.GONE
