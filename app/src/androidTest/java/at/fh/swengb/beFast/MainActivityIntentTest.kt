e9412e2fa8cab9f7757439043c9af10d0549a04d
Lukas Dworacek <lukas@dworacek.at>
2021-01-21 11:28:04 +0100

    Layout Fix & new TestName

--- app/src/androidTest/java/at/fh/swengb/beFast/MainActivityIntentTest.kt
+++ app/src/androidTest/java/at/fh/swengb/beFast/MainActivityIntentTest.kt
@@ -3,16 +3,22 @@ package at.fh.swengb.beFast
 import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import at.fh.swengb.beFast.MainActivity
 import at.fh.swengb.beFast.R
+import at.fh.swengb.beFast.models.tweets.TweetsItem
+import at.fh.swengb.beFast.news.NewsFragment
+import at.fh.swengb.beFast.news.recyclerview.TweetAdapter
+import at.fh.swengb.beFast.news.recyclerview.TweetViewHolder
 import at.fh.swengb.beFast.settings.SettingsActivity
+import kotlinx.android.synthetic.main.news_recycler_view_item.view.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -34,7 +40,7 @@ class MainActivityIntentTest {
     }
 
     @Test
-    fun clickingLessonsButton_shouldLaunchLessonListActivity() {
+    fun clickingSettingsButton_shouldLaunchSettingsActivity() {
         onView(withId(R.id.settings)).perform(click())
         intended(hasComponent(SettingsActivity::class.java.name))
     }
