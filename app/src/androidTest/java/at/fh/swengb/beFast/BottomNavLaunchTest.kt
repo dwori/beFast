1fcdb90f9e51562f7092c80c8a24e3bccedde5cd
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2021-01-28 22:38:37 +0100

    Android Testing

---
+++ app/src/androidTest/java/at/fh/swengb/beFast/BottomNavLaunchTest.kt
@@ -0,0 +1,49 @@
+package at.fh.swengb.beFast
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import at.fh.swengb.beFast.settings.SettingsActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BottomNavLaunchTest {
+        @get:Rule
+        val rule = ActivityScenarioRule(MainActivity::class.java)
+
+        @Before
+        fun setup() {
+            Intents.init()
+        }
+        @After
+        fun tearDown() {
+            Intents.release()
+        }
+
+        @Test
+        fun clickingBrands_shouldLaunchBrandsFragment() {
+            Espresso.onView(ViewMatchers.withId(R.id.navigation_brands)).perform(ViewActions.click())
+            Espresso.onView(ViewMatchers.withId(R.id.fragment_brands)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        }
+
+        @Test
+        fun clickingDrops_shouldLaunchDropsFragment() {
+            Espresso.onView(ViewMatchers.withId(R.id.navigation_drops)).perform(ViewActions.click())
+            Espresso.onView(ViewMatchers.withId(R.id.fragment_drops_recyclerview)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        }
+
+        @Test
+        fun clickingMore_shouldLaunchMoreFragment() {
+            Espresso.onView(ViewMatchers.withId(R.id.navigation_more)).perform(ViewActions.click())
+            Espresso.onView(ViewMatchers.withId(R.id.fragment_more)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        }
+}
---
+++ app/src/androidTest/java/at/fh/swengb/beFast/UserSavesNoteTest.kt
@@ -0,0 +1,58 @@
+package at.fh.swengb.beFast
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import at.fh.swengb.beFast.drops.recyclerview.DropViewHolder
+import at.fh.swengb.beFast.drops.recyclerview.description.DescriptionActivity
+import at.fh.swengb.beFast.drops.recyclerview.description.DescriptionNoteActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class UserSavesNoteTest {
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+    @Test
+    fun UserSavesANote() {
+        Espresso.onView(withId(R.id.navigation_drops)).perform(ViewActions.click())
+
+        Thread.sleep(2000)
+        Espresso.onView(withId(R.id.fragment_drops_recyclerview))
+                .perform(
+                        RecyclerViewActions.actionOnItem<DropViewHolder>(
+                                ViewMatchers.hasDescendant(
+                                        ViewMatchers.withText("Air Force 1 Rayguns")
+                                ), ViewActions.click()
+                        )
+                )
+        Intents.intended(hasComponent(DescriptionActivity::class.java.name))
+        Thread.sleep(2000)
+        Espresso.onView(withId(R.id.description_name)).check(ViewAssertions.matches(ViewMatchers.withText("Air Force 1 Rayguns")))
+
+        Espresso.onView(withId(R.id.description_note)).perform(ViewActions.click())
+        Intents.intended(hasComponent(DescriptionNoteActivity::class.java.name))
+
+        Espresso.onView(withId(R.id.save_note)).perform(ViewActions.click())
+        Intents.intended(hasComponent(DescriptionActivity::class.java.name))
+    }
+}
